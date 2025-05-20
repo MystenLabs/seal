e77c8aacf855ea14bbf6b656185b169c03af6c21
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-05-20 12:43:01 +0200

    simplify

--- crates/key-server/src/package_info.rs
+++ crates/key-server/src/package_info.rs
@@ -36,26 +36,19 @@ pub async fn fetch_package_info(
         call_with_duration(metrics.map(|m| &m.fetch_pkg_ids_duration), || async {
             fetch_first_and_last_pkg_id(&pkg_id, network).await
         })
-            .await?;
-    match mvr_name {
-        Some(mvr_name) => {
-            // Check that the MVR name points to the first version of the given package
-            let mvr_reference = mvr_forward_resolution(sui_client, &mvr_name).await?;
-            if mvr_reference != first {
-                return Err(InvalidMVRName);
-            }
-            Ok(PackageInfo {
-                first,
-                latest,
-                mvr_name: Some(mvr_name),
-            })
-        }
-        None => {
-            Ok(PackageInfo {
-                first,
-                latest,
-                mvr_name: None,
-            })
+        .await?;
+
+    if let Some(mvr_name) = mvr_name.as_ref() {
+        // Check that the MVR name points to the first version of the given package
+        let mvr_reference = mvr_forward_resolution(sui_client, mvr_name).await?;
+        if mvr_reference != first {
+            return Err(InvalidMVRName);
         }
     }
+
+    Ok(PackageInfo {
+        first,
+        latest,
+        mvr_name,
+    })
 }
