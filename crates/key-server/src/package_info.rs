20ebf0b04e9f575919731f84c2f2a91ba95833c1
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-05-20 15:15:14 +0200

    license

--- crates/key-server/src/package_info.rs
+++ crates/key-server/src/package_info.rs
@@ -1,3 +1,6 @@
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::errors::InternalError;
 use crate::errors::InternalError::InvalidMVRName;
 use crate::externals::fetch_first_and_last_pkg_id;
