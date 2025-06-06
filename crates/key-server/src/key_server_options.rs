3855ee6cf303a5b77bded454b4d1c8d6a55a4e3e
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-06-06 12:26:54 +0200

    clean up

--- crates/key-server/src/key_server_options.rs
+++ crates/key-server/src/key_server_options.rs
@@ -11,17 +11,22 @@ use sui_types::base_types::ObjectID;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct KeyServerOptions {
+    // TODO: remove this when the legacy key server is no longer needed
+    /// The object ID of the legacy key server object.
+    pub legacy_key_server_object_id: ObjectID,
+
+    /// The object ID of the key server object.
+    pub key_server_object_id: ObjectID,
+
+    /// The network this key server is running on. Defaults to Testnet.
     #[serde(default = "default_network")]
     pub network: Network,
 
     /// The minimum version of the SDK that is required to use this service.
+    #[serde(default = "default_sdk_version_requirement")]
     pub sdk_version_requirement: VersionReq,
 
-    // TODO: remove this when the legacy key server is no longer needed
-    pub legacy_key_server_object_id: ObjectID,
-
-    pub key_server_object_id: ObjectID,
-
+    #[serde(default = "default_metrics_host_port")]
     pub metrics_host_port: u16,
 
     /// The interval at which the latest checkpoint timestamp is updated.
@@ -63,10 +68,10 @@ impl KeyServerOptions {
     ) -> Self {
         Self {
             network,
-            sdk_version_requirement: VersionReq::parse(">=0.4.5").unwrap(),
+            sdk_version_requirement: default_sdk_version_requirement(),
             legacy_key_server_object_id,
             key_server_object_id,
-            metrics_host_port: 9184,
+            metrics_host_port: default_metrics_host_port(),
             checkpoint_update_interval: default_checkpoint_update_interval(),
             rgp_update_interval: default_rgp_update_interval(),
             allowed_staleness: default_allowed_staleness(),
@@ -95,6 +100,14 @@ fn default_allowed_staleness() -> Duration {
     from_mins(2)
 }
 
+fn default_metrics_host_port() -> u16 {
+    9184
+}
+
+fn default_sdk_version_requirement() -> VersionReq {
+    VersionReq::parse(">=0.4.5").expect("Failed to parse default SDK version requirement")
+}
+
 #[test]
 fn test_parse_config() {
     use std::str::FromStr;
@@ -120,7 +133,7 @@ fn test_parse_config() {
     assert_eq!(options.checkpoint_update_interval, Duration::from_secs(13));
 
     let valid_configuration_custom_network =
-        "network: !Custom\n  graphql_url: https://graphql.dk\n  node_url: https://node.dk\nsdk_version_requirement: '>=0.2.7'\nmetrics_host_port: 0\nlegacy_key_server_object_id: '0x0'\nkey_server_object_id: '0x0'\n";
+        "network: !Custom\n  graphql_url: https://graphql.dk\n  node_url: https://node.dk\nlegacy_key_server_object_id: '0x0'\nkey_server_object_id: '0x0'\n";
     let options: KeyServerOptions = serde_yaml::from_str(valid_configuration_custom_network)
         .expect("Failed to parse valid configuration");
     assert_eq!(
@@ -131,7 +144,6 @@ fn test_parse_config() {
         }
     );
 
-    let missing_port =
-        "network: Mainnet\nsdk_version_requirement: '>=0.2.7'\nlegacy_key_server_object_id: '0x0'\nkey_server_object_id: '0x0'\n";
-    assert!(serde_yaml::from_str::<KeyServerOptions>(missing_port).is_err());
+    let missing_object_id = "legacy_key_server_object_id: '0x0'\n";
+    assert!(serde_yaml::from_str::<KeyServerOptions>(missing_object_id).is_err());
 }
