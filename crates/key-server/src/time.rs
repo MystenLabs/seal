b0cf0e687229457a6487ff96171dc84fd5fda357
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-06-30 13:52:34 +0200

    fix tests

--- crates/key-server/src/time.rs
+++ crates/key-server/src/time.rs
@@ -74,8 +74,9 @@ mod tests {
         let now = current_epoch_time();
         // 30th of June 2025 10:19:00
         assert!(now > 1751271540000);
-        thread::sleep(Duration::from_secs(2));
-        assert!(current_epoch_time() >= now + 2000);
+        thread::sleep(Duration::from_secs(1));
+        let now2 = current_epoch_time();
+        assert!(now2 >= now + 1000 && now2 < now + 1100);
     }
 
     #[test]
@@ -83,7 +84,7 @@ mod tests {
         let now = current_epoch_time();
         let offset_in_past = now - 1000;
         let (duration, is_past) = duration_since(offset_in_past);
-        assert!(duration >= 1000 && duration < 1100);
+        assert!((1000..=1100).contains(&duration));
         assert!(is_past);
         let checked_duration = checked_duration_since(offset_in_past);
         assert!(
@@ -102,7 +103,7 @@ mod tests {
         let now = current_epoch_time();
         let offset_in_future = now + 1000;
         let (duration, is_past) = duration_since(offset_in_future);
-        assert!(duration > 900 && duration <= 1000);
+        assert!((900..=1000).contains(&duration));
         assert!(!is_past);
         let checked_duration = checked_duration_since(offset_in_future);
         assert!(checked_duration.is_none());
