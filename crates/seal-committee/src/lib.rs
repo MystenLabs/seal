bd08f8b11d3d264d377606dc46be3e88c62fb969
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2025-10-31 09:12:34 -0700

    [commitee] add seal-committee crate with grpc and helper funcs (#376)

---
+++ crates/seal-committee/src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod grpc_helper;
+pub mod move_types;
+pub mod types;
+
+pub use grpc_helper::{create_grpc_client, fetch_committee_data};
+pub use move_types::{CommitteeState, MemberInfo, ParsedMemberInfo, SealCommittee, VecMap};
+pub use types::Network;
