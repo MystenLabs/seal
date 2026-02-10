1045360a71b1e1341cc81885f5302ccfb93becb6
Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>
2026-02-10 13:44:48 -0600

    docs: Migrate documentation from mkDocs to Docusaurus (#468)

---
+++ docs/site/docusaurus.config.js
@@ -0,0 +1,111 @@
+/*
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+*/
+
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Seal Documentation',
+  tagline: 'This is a Walrus Site for Seal documentation.',
+  favicon: 'img/favicon.ico',
+  headTags: [
+    {
+      tagName: "meta",
+      attributes: {
+        name: "algolia-site-verification",
+        content: "BCA21DA2879818D2",
+      },
+    },
+  ],
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  url: 'https://seal-docs.wal.app',
+  baseUrl: '/',
+  
+  organizationName: 'Mysten Labs',
+  projectName: 'seal',
+
+  onBrokenLinks: 'throw',
+  onBrokenAnchors: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  
+  markdown: {
+    format: "detect",
+    mermaid: true,
+  },
+
+  clientModules: [require.resolve("./src/client/pushfeedback-toc.js")],
+  
+  plugins: [
+    //require.resolve('./src/plugins/framework'),
+    "docusaurus-plugin-copy-page-button",
+    [
+    "docusaurus-plugin-plausible",
+      {
+        domain: 'https://seal-docs.wal.app',
+      },
+    ],
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: '../content',
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        logo: {
+          alt: 'Seal Logo',
+          src: 'img/logo.svg',
+          href: '/',
+        },
+        items: [
+          {
+            href: 'https://github.com/MystenLabs/seal',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
