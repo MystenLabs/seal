5c1238c15349183d071c5efd27006f5645b86709
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2026-02-17 10:05:14 -0500

    [release] 0.6.1

--- crates/key-server/src/aggregator/server.rs
+++ crates/key-server/src/aggregator/server.rs
@@ -65,7 +65,7 @@ fn default_ts_sdk_version_requirement() -> VersionReq {
 
 /// Default key server version requirement.
 fn default_key_server_version_requirement() -> VersionReq {
-    VersionReq::parse(">=0.6.0").expect("Failed to parse default key server version requirement")
+    VersionReq::parse(">=0.6.1").expect("Failed to parse default key server version requirement")
 }
 
 /// API credentials for a key server.
