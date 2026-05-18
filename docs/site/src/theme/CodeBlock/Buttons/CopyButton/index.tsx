a4e99b32a2dbf6c9266136a86f56b9bbe7e2f260
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-05-18 14:05:16 -0500

    feat: wire OpenInAgentButton into CopyButton

---
+++ docs/site/src/theme/CodeBlock/Buttons/CopyButton/index.tsx
@@ -0,0 +1,16 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { type ReactNode } from "react";
+import OriginalCopyButton from "@theme-original/CodeBlock/Buttons/CopyButton";
+import type { Props } from "@theme/CodeBlock/Buttons/CopyButton";
+import OpenInAgentButton from "../../../../shared/components/OpenInAgentButton";
+
+export default function CopyButton(props: Props): ReactNode {
+  return (
+    <>
+      <OriginalCopyButton {...props} />
+      <OpenInAgentButton className={props.className} />
+    </>
+  );
+}
