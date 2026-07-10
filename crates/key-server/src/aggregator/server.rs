e0d4c5f476f12dccec739cc03787096969174287
benr-ml <benriva@mystenlabs.com>
2026-07-10 11:43:56 +0300

    fix tests

--- crates/key-server/src/aggregator/server.rs
+++ crates/key-server/src/aggregator/server.rs
@@ -995,12 +995,8 @@ mod tests {
         let registry = Registry::new();
         let metrics = Arc::new(AggregatorMetrics::new(&registry));
         let grpc_client = SuiGrpcClient::new(options.node_url()).unwrap();
-        let sui_client = SuiClientBuilder::default()
-            .build(&options.node_url())
-            .await
-            .unwrap();
-        let sui_rpc_client = SuiRpcClient::new(
-            sui_client,
+        let sui_rpc_client = SuiRpcClient::new_with_optional_sui_client(
+            None,
             grpc_client,
             options.rpc_config.retry_config.clone(),
             None,
